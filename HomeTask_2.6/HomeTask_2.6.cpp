3effa4a78e1123f9ef7405031fcfe36f8c19a532
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-14 23:51:02 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_2.6/HomeTask_2.6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int num, n1, i, numnew;
+    cout << "Введите число:\n";
+    cin >> num;
+    i = 0;
+    numnew = 0;
+    while (num > 0)
+    {
+        n1 = num % 10;
+        if ((n1 != 3) && (n1 != 6))
+        {
+            numnew = numnew + n1 * pow(10, i);
+            i++;
+        }
+
+        num = num / 10;
+    }
+    cout << "Новое число: " << numnew;
+
+}
+
